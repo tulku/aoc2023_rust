60321d4f467d565c924d7c86a6070c72218a9f1b
Michel Hidalgo <michel@ekumenlabs.com>
2023-12-02 17:51:07 -0300

    Done with day 1, part 2
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- day-01/calib/src/main.rs
+++ day-01/calib/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
-use std::char;
 use std::path;
+
+use lazy_static::lazy_static;
 
 use clap::Parser;
 
@@ -12,16 +13,56 @@ struct CLI {
     path: path::PathBuf,
 }
 
+lazy_static! {
+    static ref DECIMAL_DIGIT_NAMES: Vec<&'static str> = vec![
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+    ];
+}
+
+fn find_decimal_digit(string: &str) -> Option<u32> {
+    let mut digit_candidates: Vec<_> = (0..10).map(|digit| {
+        match string.find(DECIMAL_DIGIT_NAMES[digit]) {
+            Some(index) => Some((index, digit as u32)),
+            _ => None
+        }
+    }).collect();
+    digit_candidates.push(
+        match string.find(char::is_numeric) {
+            Some(index) => {
+                let character = string.chars().nth(index).unwrap();
+                Some((index, character.to_digit(10).unwrap()))
+            },
+            _ => None
+        }
+    );
+    digit_candidates.iter().flatten().min().map(|(_, digit)| digit).copied()
+}
+
+fn rfind_decimal_digit(string: &str) -> Option<u32> {
+    let mut digit_candidates: Vec<_> = (0..10).map(|digit| {
+        match string.rfind(DECIMAL_DIGIT_NAMES[digit]) {
+            Some(index) => Some((index, digit as u32)),
+            _ => None
+        }
+    }).collect();
+    digit_candidates.push(
+        match string.rfind(char::is_numeric) {
+            Some(index) => {
+                let character = string.chars().nth(index).unwrap();
+                Some((index, character.to_digit(10).unwrap()))
+            },
+            _ => None
+        }
+    );
+    digit_candidates.iter().flatten().max().map(|(_, digit)| digit).copied()
+}
+
 fn parse_calibration_number(line: &str) -> Result<u32, &str> {
-    match line.find(char::is_numeric) {
-        Some(leading_index) => {
-            let trailing_index = line.rfind(char::is_numeric).unwrap();
-            let leading_char = line.chars().nth(leading_index).unwrap();
-            let trailing_char = line.chars().nth(trailing_index).unwrap();
-            let leading_number = leading_char.to_digit(10).unwrap();
-            let trailing_number = trailing_char.to_digit(10).unwrap();
-            Ok(leading_number * 10 + trailing_number)
-        },
+    match find_decimal_digit(line) {
+        Some(leading_digit) => {
+            let trailing_digit = rfind_decimal_digit(line).unwrap();
+            Ok(leading_digit * 10 + trailing_digit)
+        }
         None => Err("missing number")
     }
 }
@@ -38,4 +79,23 @@ fn main() {
         )).sum::<u32>();
 
     println!("{}", sum);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn calibration_number_parsing() {
+        assert_eq!(29, parse_calibration_number("two1nine").unwrap());
+        assert_eq!(83, parse_calibration_number("eightwothree").unwrap());
+        assert_eq!(13, parse_calibration_number("abcone2threexyz").unwrap());
+        assert_eq!(24, parse_calibration_number("xtwone3four").unwrap());
+        assert_eq!(42, parse_calibration_number("4nineeightseven2").unwrap());
+        assert_eq!(14, parse_calibration_number("zoneight234").unwrap());
+        assert_eq!(76, parse_calibration_number("7pqrstsixteen").unwrap());
+        assert_eq!(22, parse_calibration_number("2q").unwrap());
+        assert_eq!(11, parse_calibration_number("one").unwrap());
+        assert_eq!(18, parse_calibration_number("1threeight").unwrap());
+    }
 }
