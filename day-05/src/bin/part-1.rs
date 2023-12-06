e7cb0696c9a40c2989b806d5ce060b7d12c8cbff
Lucas Chiesa <lucas.chiesa@contractor.kaercher.com>
2023-12-06 09:46:54 +0100

    Day 5 part 1

--- day-05/src/bin/part-1.rs
+++ day-05/src/bin/part-1.rs
@@ -1,3 +1,13 @@
+use day_05::parse_almanac;
+
+fn part_1(input: &str) -> usize {
+    let almanac = parse_almanac(input);
+    let locations = almanac.get_all_locations();
+    *locations.iter().min().unwrap()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../../input/input-1.txt");
+    let min_location = part_1(input);
+    println!("Min location: {}", min_location);
 }
--- day-05/src/lib.rs
+++ day-05/src/lib.rs
@@ -1,58 +1,113 @@
-use std::collections::HashMap;
-
 use regex::Regex;
+
+#[derive(Debug)]
+struct MapRange {
+    destination_start: usize,
+    source_start: usize,
+    length: usize,
+}
+
+impl MapRange {
+    fn new(destination_start: usize, source_start: usize, length: usize) -> Self {
+        MapRange {
+            destination_start,
+            source_start,
+            length,
+        }
+    }
+
+    fn map(&self, input: usize) -> Option<usize> {
+        if input < self.source_start || input > self.source_start + self.length - 1 {
+            return None;
+        }
+        let offset = input - self.source_start;
+        Some(self.destination_start + offset)
+    }
+}
+
+#[derive(Debug)]
+struct RangedMap {
+    ranges: Vec<MapRange>,
+}
+
+impl RangedMap {
+    fn new() -> Self {
+        RangedMap { ranges: Vec::new() }
+    }
+
+    fn from_ranges(numbers: Vec<usize>) -> Self {
+        let mut ranges = Vec::new();
+        for map_set in numbers.chunks(3) {
+            let destination_start = map_set[0];
+            let source_start = map_set[1];
+            let length = map_set[2];
+            ranges.push(MapRange::new(destination_start, source_start, length));
+        }
+        RangedMap { ranges }
+    }
+
+    fn map(&self, input: usize) -> usize {
+        for range in &self.ranges {
+            if let Some(output) = range.map(input) {
+                return output;
+            }
+        }
+        input
+    }
+}
+
 #[derive(Debug)]
 pub struct Almanac {
     seeds: Vec<usize>,
-    seeds_to_soil: HashMap<usize, usize>,
-    soil_to_fertilizer: HashMap<usize, usize>,
-    fertilizer_to_water: HashMap<usize, usize>,
-    water_to_light: HashMap<usize, usize>,
-    light_to_temperature: HashMap<usize, usize>,
-    temperature_to_humidity: HashMap<usize, usize>,
-    humidity_to_location: HashMap<usize, usize>,
+    seeds_to_soil: RangedMap,
+    soil_to_fertilizer: RangedMap,
+    fertilizer_to_water: RangedMap,
+    water_to_light: RangedMap,
+    light_to_temperature: RangedMap,
+    temperature_to_humidity: RangedMap,
+    humidity_to_location: RangedMap,
 }
 
 impl Almanac {
     fn new() -> Self {
         Almanac {
             seeds: Vec::new(),
-            seeds_to_soil: HashMap::new(),
-            soil_to_fertilizer: HashMap::new(),
-            fertilizer_to_water: HashMap::new(),
-            water_to_light: HashMap::new(),
-            light_to_temperature: HashMap::new(),
-            temperature_to_humidity: HashMap::new(),
-            humidity_to_location: HashMap::new(),
+            seeds_to_soil: RangedMap::new(),
+            soil_to_fertilizer: RangedMap::new(),
+            fertilizer_to_water: RangedMap::new(),
+            water_to_light: RangedMap::new(),
+            light_to_temperature: RangedMap::new(),
+            temperature_to_humidity: RangedMap::new(),
+            humidity_to_location: RangedMap::new(),
         }
     }
 
     pub fn seed_to_soil(&self, seed: usize) -> usize {
-        *self.seeds_to_soil.get(&seed).unwrap_or(&seed)
+        self.seeds_to_soil.map(seed)
     }
 
     pub fn soil_to_fertilizer(&self, soil: usize) -> usize {
-        *self.soil_to_fertilizer.get(&soil).unwrap_or(&soil)
+        self.soil_to_fertilizer.map(soil)
     }
 
     pub fn fertilizer_to_water(&self, input: usize) -> usize {
-        *self.fertilizer_to_water.get(&input).unwrap_or(&input)
+        self.fertilizer_to_water.map(input)
     }
 
     pub fn water_to_light(&self, input: usize) -> usize {
-        *self.water_to_light.get(&input).unwrap_or(&input)
+        self.water_to_light.map(input)
     }
 
     pub fn light_to_temperature(&self, input: usize) -> usize {
-        *self.light_to_temperature.get(&input).unwrap_or(&input)
+        self.light_to_temperature.map(input)
     }
 
     pub fn temperature_to_humidity(&self, input: usize) -> usize {
-        *self.temperature_to_humidity.get(&input).unwrap_or(&input)
+        self.temperature_to_humidity.map(input)
     }
 
     pub fn humidity_to_location(&self, input: usize) -> usize {
-        *self.humidity_to_location.get(&input).unwrap_or(&input)
+        self.humidity_to_location.map(input)
     }
 
     pub fn seed_to_location(&self, seed: usize) -> usize {
@@ -64,6 +119,14 @@ impl Almanac {
         let humidity = self.temperature_to_humidity(temperature);
         let location = self.humidity_to_location(humidity);
         location
+    }
+
+    pub fn get_all_locations(&self) -> Vec<usize> {
+        let mut locations = Vec::new();
+        for seed in &self.seeds {
+            locations.push(self.seed_to_location(*seed));
+        }
+        locations
     }
 }
 
@@ -77,17 +140,8 @@ fn get_numbers(line: &str) -> Vec<usize> {
     numbers
 }
 
-fn build_map(numbers: Vec<usize>) -> HashMap<usize, usize> {
-    let mut map = HashMap::new();
-    for map_set in numbers.chunks(3) {
-        let destination_start = map_set[0];
-        let source_start = map_set[1];
-        let length = map_set[2];
-        for i in 0..length - 1 {
-            map.insert(source_start + i, destination_start + i);
-        }
-    }
-    map
+fn build_map(numbers: Vec<usize>) -> RangedMap {
+    RangedMap::from_ranges(numbers)
 }
 
 pub fn parse_almanac(almanac: &str) -> Almanac {
@@ -97,7 +151,6 @@ pub fn parse_almanac(almanac: &str) -> Almanac {
         let label = captures.name("label").unwrap().as_str();
         let numbers = str::replace(captures.name("numbers").unwrap().as_str(), "\n", " ");
         let numbers = get_numbers(numbers.as_str());
-        println!("Label {}", label);
         match label {
             "seeds" => parsed_almanac.seeds = numbers,
             "seed-to-soil map" => parsed_almanac.seeds_to_soil = build_map(numbers),
@@ -131,8 +184,11 @@ mod tests {
 
         assert_eq!(almanac.seed_to_location(79), 82);
         assert_eq!(almanac.seed_to_location(55), 86);
-        // assert_eq!(almanac.seed_to_location(14), 43);
-        // assert_eq!(almanac.seed_to_location(13), 35);
+        assert_eq!(almanac.seed_to_location(14), 43);
+        assert_eq!(almanac.seed_to_location(13), 35);
+
+        let locations = almanac.get_all_locations();
+        assert_eq!(locations.iter().min(), Some(&35));
     }
 
     #[test]
